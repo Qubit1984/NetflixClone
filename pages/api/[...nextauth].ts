8cbc4168ccf00f0ff4b11216d41d7284164a5ba9
Qubit1984 <qubitphy@gmail.com>
2023-04-24 16:41:40 +0800

    ..

--- pages/api/[...nextauth].ts
+++ pages/api/[...nextauth].ts
@@ -2,7 +2,7 @@ import NextAuth, { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@/next-auth/prisma-adapter";
+//import { PrismaAdapter } from "@/next-auth/prisma-adapter";
 import { compare } from "bcrypt";
 import prismadb from "@/lib/prismadb";
 
@@ -61,7 +61,7 @@ export const authOptions: AuthOptions = {
     signIn: "/auth",
   },
   debug: process.env.NODE_ENV === "development",
-  adapter: PrismaAdapter(prismadb),
+  //adapter: PrismaAdapter(prismadb),
   session: { strategy: "jwt" },
   jwt: {
     secret: process.env.NEXTAUTH_JWT_SECRET,
